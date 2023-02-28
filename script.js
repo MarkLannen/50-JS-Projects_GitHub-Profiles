2b857e7d5e3d5bb158a552c2dd427d6e18e5cc2c
Mark Lannen <marklannendev@gmail.com>
2023-02-28 11:47:04 -0700

    add fetch for individual repos and add them to profile card

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
     createUserCard(data);
+    getRepos(username);
   } catch (err) {
     if (err.response.status == 404) {
       createErrorCard('No profile with this username');
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created');
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard('Problem fetching repos');
   }
 }
 
@@ -34,9 +44,7 @@ function createUserCard(user) {
             <li>${user.public_repos}<strong> Repos</strong></li>
           </ul>
           <div id="repos">
-            <a href="#" class="repo">Repo 1</a>
-            <a href="#" class="repo">Repo 2</a>
-            <a href="#" class="repo">Repo 3</a>
+            
           </div>
         </div>
       </div>`;
@@ -50,6 +58,20 @@ function createErrorCard(msg) {
       </div>
 `;
   main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  repos.slice(0, 10).forEach((repo) => {
+    const repoEl = document.createElement('a');
+
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.target = '_blank';
+    repoEl.innerText = repo.name;
+    reposEl.appendChild(repoEl);
+  });
 }
 
 form.addEventListener('submit', (e) => {
